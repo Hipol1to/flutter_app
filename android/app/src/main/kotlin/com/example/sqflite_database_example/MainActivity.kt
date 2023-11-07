fad9f217ffce328b9056b3f6347260df926c2c08
Hipol1to <hipolitoprz2001@gmail.com>
2023-11-06 21:43:26 -0400

    photo uploading added

---
+++ android/app/src/main/kotlin/com/example/sqflite_database_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.war_20187078
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/note.dart
@@ -0,0 +1,75 @@
+final String tableNotes = 'notes';
+
+class NoteFields {
+  static final List<String> values = [
+    /// Agregar to lo campo
+    id, isImportant, number, title, description, time, imagePath
+  ];
+
+  static final String id = '_id';
+  static final String isImportant = 'isImportant';
+  static final String number = 'number';
+  static final String title = 'title';
+  static final String description = 'description';
+  static final String time = 'time';
+  static final String imagePath = 'imagePath';
+}
+
+class Note {
+  final int? id;
+  final bool isImportant;
+  final int number;
+  final String title;
+  final String description;
+  final DateTime createdTime;
+  final String imagePath;
+
+  const Note({
+    this.id,
+    required this.isImportant,
+    required this.number,
+    required this.title,
+    required this.description,
+    required this.createdTime,
+    required this.imagePath,
+  });
+
+  Note copy({
+    int? id,
+    bool? isImportant,
+    int? number,
+    String? title,
+    String? description,
+    DateTime? createdTime,
+    String? imagePath,
+  }) =>
+      Note(
+        id: id ?? this.id,
+        isImportant: isImportant ?? this.isImportant,
+        number: number ?? this.number,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        createdTime: createdTime ?? this.createdTime,
+        imagePath: imagePath ?? this.imagePath,
+      );
+
+  static Note fromJson(Map<String, Object?> json) => Note(
+        id: json[NoteFields.id] as int?,
+        isImportant: json[NoteFields.isImportant] == 1,
+        number: json[NoteFields.number] as int,
+        title: json[NoteFields.title] as String,
+        description: json[NoteFields.description] as String,
+        createdTime: DateTime.parse(json[NoteFields.time] as String),
+        imagePath: json[NoteFields.imagePath] as String,
+      );
+
+  Map<String, Object?> toJson() => {
+        NoteFields.id: id,
+        NoteFields.title: title,
+        NoteFields.isImportant: isImportant ? 1 : 0,
+        NoteFields.number: number,
+        NoteFields.description: description,
+        NoteFields.time: createdTime.toIso8601String(),
+        NoteFields.imagePath: imagePath,
+      };
+}
